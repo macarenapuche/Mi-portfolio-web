73cdd28619565e0814c4fa4b06e18544d690b4df
macarenapuche <macupuche@hotmail.com>
2023-01-12 09:52:29 +0100

    Agregado de NgCircleProgressModule (not working)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+import { NgCircleProgressModule } from 'ng-circle-progress/public-api';
+import { HysSkillsComponent } from './components/hys-skills/hys-skills.component';
 
 @NgModule({
   declarations: [
@@ -14,12 +16,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HeaderComponent,
     BodyComponent,
     LoginComponent,
+    HysSkillsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    // NgCircleProgressModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
